9e21aef9501500f5ec20fece12a49e0b0c7e84e8
aneeshack <aneeshack08@gmail.com>
2025-04-05 11:58:41 +0530

    feat: deployment issues resolved v2

--- backEnd/src/controllers/adminController.ts
+++ backEnd/src/controllers/adminController.ts
@@ -18,7 +18,8 @@ export class AdminController{
     }
 
     async rejectedInstructors(req: Request, res:Response):Promise<void>{
-        try {
+        try { 
+            
             const allRequsts = await this._adminService.getAllRejectedRequests()
             res.status(201).json({ success: true, data:allRequsts });
         } catch (error: unknown) {
